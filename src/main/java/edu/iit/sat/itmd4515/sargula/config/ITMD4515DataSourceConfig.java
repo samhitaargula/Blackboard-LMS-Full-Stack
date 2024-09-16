f8c33b1ae270d8a7e794e6d182957841358b5f52
Samhita Argula <samhita252@gmail.com>
2024-09-16 01:28:51 -0500

    Lab 3 - Adding persistence files

---
+++ src/main/java/edu/iit/sat/itmd4515/sargula/config/ITMD4515DataSourceConfig.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.iit.sat.itmd4515.sargula.config;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+
+/**
+ *
+ * @author sargula
+ */
+
+@DataSourceDefinition(
+        name = "java:app/jdbc/itmd4515DS",
+        className = "com.mysql.cj.jdbc.MysqlDataSource",
+        serverName = "localhost",
+        portNumber = 3306,
+        databaseName = "sakila",
+        user = "itmd4515",
+        password = "itmd4515",
+        properties = {
+            "zeroDateTimeBehavior=CONVERT_TO_NULL",
+            "useSSL=false"
+        }
+)
+public class ITMD4515DataSourceConfig {
+    // jdbc:mysql://localhost:3306/itmd4515?zeroDateTimeBehavior=CONVERT_TO_NULL
+}
