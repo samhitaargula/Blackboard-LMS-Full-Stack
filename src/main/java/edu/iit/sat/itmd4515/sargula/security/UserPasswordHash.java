169cfc251c29de4608f731232eb35beac8140887
Samhita Argula <samhita252@gmail.com>
2024-11-04 17:50:46 -0600

    Lab 8 - Adding hashing

---
+++ src/main/java/edu/iit/sat/itmd4515/sargula/security/UserPasswordHash.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.iit.sat.itmd4515.sargula.security;
+
+import jakarta.inject.Inject;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.security.enterprise.identitystore.Pbkdf2PasswordHash;
+
+/**
+ *
+ * @author sargula
+ */
+public class UserPasswordHash {
+    @Inject private Pbkdf2PasswordHash hash;
+    
+    @PrePersist
+    @PreUpdate
+    private void hashPassword(User u){
+        u.setPassword(hash.generate(u.getPassword().toCharArray()));
+    }
+}
