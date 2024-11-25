5c8379ed3808b3fdba9bb2529458911a7fa2f208
Samhita Argula <samhita252@gmail.com>
2024-11-25 13:45:11 -0600

    Lab 10 - Adding lesson service

--- src/main/java/edu/iit/sat/itmd4515/sargula/service/LessonService.java
+++ src/main/java/edu/iit/sat/itmd4515/sargula/service/LessonService.java
@@ -4,8 +4,10 @@
  */
 package edu.iit.sat.itmd4515.sargula.service;
 
+import edu.iit.sat.itmd4515.sargula.domain.Assignment;
 import edu.iit.sat.itmd4515.sargula.domain.Lesson;
 import edu.iit.sat.itmd4515.sargula.domain.LessonType;
+import edu.iit.sat.itmd4515.sargula.domain.Teacher;
 import jakarta.ejb.Stateless;
 import jakarta.inject.Named;
 import java.util.List;
@@ -39,5 +41,30 @@ public class LessonService extends AbstractService<Lesson>{
      */
     public LessonType[] getAllLessonTypes() {
         return LessonType.values();
+    }
+    
+    public void editLessonForAuthenticatedTeacher(Lesson lesson, Teacher student) {
+        Lesson managedRef = em.getReference(Lesson.class, lesson.getId());
+        
+        managedRef.setTitle(lesson.getTitle());
+        managedRef.setLessonDate(lesson.getLessonDate());
+        managedRef.setType(lesson.getType());
+        
+        em.merge(managedRef);
+    }
+    
+    public void deleteLessonForAuthenticatedTeacher(Lesson lesson, Teacher teacher) {
+        Teacher managedTeacherRef = em.getReference(Teacher.class, teacher.getId());
+        Lesson managedLessonRef = em.getReference(Lesson.class, lesson.getId());
+        
+        managedTeacherRef.removeTeacherLesson(managedLessonRef);
+        em.merge(managedTeacherRef);
+        
+        for( Assignment a : em.createNamedQuery("Assignment.findAssignmentsForLesson", Assignment.class).setParameter("lessonid", managedLessonRef.getId()).getResultList() ){
+            a.deleteAssignment();
+            em.remove(a);
+        }
+        
+        em.remove(managedLessonRef);
     }
 }
